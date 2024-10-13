3d0f7aa8a40f15c06abfd2151fa9d8c737e2bed1
kao quee <azxcvbnm512@yahoo.com.tw>
2024-10-13 15:50:44 +0800

    [1][Add] Fix the problem of docker image and adding the ECR setup shell and ECS task definition

--- ecr-setup-image.sh
+++ ecr-setup-image.sh
@@ -1,8 +1,8 @@
 docker-compose up -d
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 835549930321.dkr.ecr.us-east-1.amazonaws.com
-docker tag video-editor-toolkits-compression-nginx:latest 835549930321.dkr.ecr.us-east-1.amazonaws.com/test:nginx
-docker tag redis:latest 835549930321.dkr.ecr.us-east-1.amazonaws.com/test:redis
-docker tag video-editor-toolkits-server:latest 835549930321.dkr.ecr.us-east-1.amazonaws.com/test:node-server
-docker push 835549930321.dkr.ecr.us-east-1.amazonaws.com/test:nginx
-docker push 835549930321.dkr.ecr.us-east-1.amazonaws.com/test:redis
-docker push 835549930321.dkr.ecr.us-east-1.amazonaws.com/test:node-server
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <account-id >.dkr.ecr.us-east-1.amazonaws.com
+docker tag video-editor-toolkits-compression-nginx:latest <account-id >.dkr.ecr.us-east-1.amazonaws.com/video-editor:nginx-v1.0.0
+docker tag redis:latest <account-id >.dkr.ecr.us-east-1.amazonaws.com/video-editor:redis-v1.0.0
+docker tag video-editor-toolkits-node-server:latest <account-id >.dkr.ecr.us-east-1.amazonaws.com/video-editor:node-server-v1.0.0
+docker push <account-id >.dkr.ecr.us-east-1.amazonaws.com/video-editor:nginx-v1.0.0
+docker push <account-id >.dkr.ecr.us-east-1.amazonaws.com/video-editor:redis-v1.0.0
+docker push <account-id >.dkr.ecr.us-east-1.amazonaws.com/video-editor:node-server-v1.0.0
